b6132e8cd5e8c9f36de60f82b69c1037cc7edab2
Anil Deshpande <anildesh82@gmail.com>
2023-08-29 14:35:42 +0530

    Basic layout applied

--- app/src/main/java/com/codetutor/countryinfoapp/MainActivity.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/MainActivity.kt
@@ -3,44 +3,160 @@ package com.codetutor.countryinfoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.codetutor.countryinfoapp.ui.theme.CountryInfoAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CountryInfoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            CountryCard()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    CountryInfoAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            CountryCard()
+        }
+    }
+}
+
+@Composable
+fun CountryCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(1.0f)
+            .padding(2.dp),
+
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(1.0f)) {
+            Column(modifier = Modifier
+                .wrapContentSize()
+                .fillMaxWidth(0.3f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
+                Box(modifier = Modifier
+                    .padding(2.dp)){
+                    val imageResId = R.drawable.`in` // Replace with your PNG image resource ID
+                    val imagePainter: Painter = painterResource(id = imageResId)
+                    Image(painter = imagePainter, contentDescription = "Country Flag")
+                }
+
+                Text(text ="India",
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f),
+                    fontFamily = FontFamily.SansSerif,
+                    textAlign = TextAlign.Center,
+                    fontSize = 50.sp)
+
+                Text(text ="New Delhi",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+            }
+
+            Column(modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .wrapContentSize(),
+
+                verticalArrangement = Arrangement.SpaceEvenly) {
+                Text(text ="Republic of India",
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+
+                Text(text ="Asia",
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+
+                Text(text ="South Asia",
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+
+                Row(modifier = Modifier
+                    .fillMaxWidth(1.0f)
+                    .padding(2.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically) {
+                    Box(modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(0.3f)){
+                        val imageResId = R.drawable.`in` // Replace with your PNG image resource ID
+                        val imagePainter: Painter = painterResource(id = imageResId)
+                        Image(painter = imagePainter, contentDescription = "Country Flag")
+                    }
+
+                    Text(text ="Indian Rupee",
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .fillMaxWidth(0.4f))
+
+                    Column(modifier = Modifier.fillMaxWidth(0.3f)) {
+
+                        Text(text ="+91",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier)
+
+                        Text(text =".in",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CountryInfoAppTheme {
-        Greeting("Android")
-    }
+    CountryCard()
 }
