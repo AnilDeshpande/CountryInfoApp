9d4c10a13d4f58fb018f39f2b15289f9ac5b978a
Anil Deshpande <anildesh82@gmail.com>
2023-09-02 23:24:29 +0530

    next line of commit for video 14, 15

--- app/src/main/java/com/codetutor/countryinfoapp/MainActivity.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -37,6 +38,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.codetutor.countryinfoapp.ui.theme.CountryInfoAppTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,18 +73,102 @@ fun CountryCard(){
             Column(modifier = Modifier
                 .fillMaxWidth(0.2f)
                 .weight(0.2f)) {
-                Text(text = "One")
-                Text(text = "Two")
-                Text(text = "Three")
+
+                Box(modifier = Modifier
+                    .width(70.dp)
+                    .height(50.dp)
+                    .padding(2.dp),
+                    contentAlignment = Alignment.Center) {
+
+                    val imageResId = R.drawable.`in`
+                    val imagePainter: Painter = painterResource(id = imageResId)
+
+                    Image(painter = imagePainter,
+                        contentDescription = "Country Flag",
+                        contentScale = ContentScale.Crop)
+                }
+
+                Text(text = "India", modifier = Modifier
+                    .fillMaxWidth(1.0f)
+                    .padding(2.dp),
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center)
+
+                Text(text = "New Delhi", modifier = Modifier
+                    .fillMaxWidth(1.0f)
+                    .padding(2.dp),
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.Center)
             }
 
             Column(modifier = Modifier
                 .fillMaxWidth(0.8f)
                 .weight(0.8f)) {
 
+                Text(text ="Republic of India",
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+
+                Text(text ="Asia",
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+
+                Text(text ="South Asia",
+                    fontSize = 12.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(1.0f))
+
+                Row(modifier = Modifier.fillMaxWidth(1.0f),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically) {
+
+                    CirculerText(text = "₹")
+
+                    Text(text ="Indian Rupee",
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .fillMaxWidth(0.4f))
+
+                    Column(modifier = Modifier.fillMaxWidth(0.3f),
+                        horizontalAlignment = Alignment.End
+                    ) {
+
+                        Text(text ="+91",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier)
+
+                        Text(text =".in",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier)
+                    }
+                }
+
             }
         }
     }
+}
+
+@Composable
+fun CirculerText(text: String){
+    Text(text = text,
+        fontSize = 12.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(0.1f)
+            .drawBehind {
+                drawCircle(color = Color.LightGray, radius = this.size.maxDimension)
+            })
 }
 
 @Preview(showBackground = true)
