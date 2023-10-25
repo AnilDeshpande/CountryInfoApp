e2f58e2f4167bce078f7767cc7e57b473eb4c1e6
Anil Deshpande <anildesh82@gmail.com>
2023-10-25 22:14:26 +0530

    MediumTopAppBar with exitUntilCollapsedScrollBehavior

--- app/src/main/java/com/codetutor/countryinfoapp/components/AppScaffold.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/components/AppScaffold.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -27,11 +28,11 @@ import com.codetutor.countryinfoapp.util.getCountryListFromJson
 fun CountryInfoAppScaffold() {
 
     var countryList = getCountryListFromJson(LocalContext.current)
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     Scaffold (
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
+            MediumTopAppBar(
                 colors = TopAppBarDefaults.smallTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
