94901a8e016ad4b9032d32c36e4d4a2b1f37179b
Anil Deshpande <anildesh82@gmail.com>
2023-10-27 00:09:15 +0530

    bottomAppBar, navigation, floating button added

--- app/src/main/java/com/codetutor/countryinfoapp/components/CountryInfoAppScaffold.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/components/CountryInfoAppScaffold.kt
@@ -1,11 +1,16 @@
 package com.codetutor.countryinfoapp.components
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Filter
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Sort
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,7 +18,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import com.codetutor.countryinfoapp.data.Country
 import com.codetutor.countryinfoapp.screens.MainScreen
@@ -23,7 +31,11 @@ import com.codetutor.countryinfoapp.util.getCountryListFromJson
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CountryInfoAppScaffold(countryList: MutableList<Country>){
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
     Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.smallTopAppBarColors(
@@ -33,23 +45,39 @@ fun CountryInfoAppScaffold(countryList: MutableList<Country>){
                 title = {
                     Text(text = "CountryInfoApp")
                 },
+//                navigationIcon = {
+//                    IconButton(onClick = { /*TODO*/ }) {
+//                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "ArrowBack")
+//                    }
+//                },
 
                 actions = {
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Filled.Filter, contentDescription = "Filter")
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Filled.Sort, contentDescription = "Sort")
-                    }
+
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "MoreVert")
                     }
-                }
+                },
+                scrollBehavior = scrollBehavior,
             )
+        },
+        bottomBar = {
+            BottomAppBar {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Sort, contentDescription = "Sort")
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ },
+                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation( )) {
+                    Icon(imageVector = Icons.Filled.Filter, contentDescription = "Filter")
+            }
         }
+
     ) { innerPaddingValues ->
         MainScreen(countryList = countryList,innerPaddingValues)
     }
