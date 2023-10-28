f8917b7c24739e7427d42311922397efdd48c8d7
Anil Deshpande <anildesh82@gmail.com>
2023-10-28 23:23:17 +0530

    adding the preview

--- app/src/main/java/com/codetutor/countryinfoapp/components/AppScaffold.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/components/AppScaffold.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codetutor.countryinfoapp.screens.MainScreen
 import com.codetutor.countryinfoapp.util.getCountryListFromJson
@@ -91,4 +92,10 @@ fun CountryInfoAppScaffold() {
     ){ innerPadding ->
         MainScreen( countryList, innerPadding = innerPadding)
     }
+}
+
+@Preview
+@Composable
+fun SimpleComposablePreview() {
+    CountryInfoAppScaffold()
 }
