c0768777891a47e3d996b5abe30eee17ee5afd01
Anil Deshpande <anildesh82@gmail.com>
2023-10-31 10:51:27 +0530

    adding scaffold inside the Theme

--- app/src/main/java/com/codetutor/countryinfoapp/MainActivity.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import com.codetutor.countryinfoapp.components.CountryInfoAppScaffold
 import com.codetutor.countryinfoapp.data.Country
 import com.codetutor.countryinfoapp.screens.MainScreen
+import com.codetutor.countryinfoapp.ui.theme.CountryInfoAppTheme
 import com.codetutor.countryinfoapp.util.getCountryListFromJson
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +16,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CountryInfoAppScaffold()
+           CountryInfoAppTheme {
+               CountryInfoAppScaffold()
+            }
         }
     }
 }
