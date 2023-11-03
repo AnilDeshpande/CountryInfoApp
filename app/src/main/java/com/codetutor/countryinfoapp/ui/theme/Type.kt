748b0d6045bf56487bb6a2169c5dc396e8b393fc
Anil Deshpande <anildesh82@gmail.com>
2023-11-03 16:09:16 +0530

    Adding custom typography

--- app/src/main/java/com/codetutor/countryinfoapp/ui/theme/Type.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/ui/theme/Type.kt
@@ -14,15 +14,16 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.ExtraBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+
+    /* Other default text styles to override
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
