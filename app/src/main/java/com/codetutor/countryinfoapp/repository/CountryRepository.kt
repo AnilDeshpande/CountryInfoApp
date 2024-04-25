542ee9427f2ce1a06ae0a1fd174c89de4b8a32cd
Anil Deshpande <anildesh82@gmail.com>
2024-04-25 12:57:40 +0530

    Slight modification of the if condition

--- app/src/main/java/com/codetutor/countryinfoapp/repository/CountryRepository.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/repository/CountryRepository.kt
@@ -2,6 +2,7 @@ package com.codetutor.countryinfoapp.repository
 
 import CountryEntity
 import android.content.Context
+import android.util.Log
 import com.codetutor.countryinfoapp.data.Country
 import com.codetutor.countryinfoapp.database.CountryDao
 import com.codetutor.countryinfoapp.util.getCountryList
@@ -14,12 +15,16 @@ class CountryRepository(private val context: Context, private val countryDao: Co
     private val contextForRepo: Context = context
 
     suspend fun fetchAndInsertAll() = withContext(Dispatchers.IO) {
-        if(getAllCountries() != null) {
+        if(getAllCountries() != null && getAllCountries().isNotEmpty()) {
+            Log.i("Room", "Countries already exist in the database ${getAllCountries()}")
             return@withContext
 
         } else {
+            Log.i("Room", "Fetching and inserting countries")
             val mutableCountryList: MutableList<Country> = getCountryList(contextForRepo)
+            Log.i("Room", "Fetched countries: $mutableCountryList")
             val countryList: List<Country> = mutableCountryList.toList()
+            Log.i("Room", "Inserting Country list: $countryList")
             countryDao.insertAll(countryList)
             return@withContext
         }
