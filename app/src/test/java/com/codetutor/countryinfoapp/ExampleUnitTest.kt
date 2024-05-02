09f9ef38d7da8081bfcf08602738158b729b835f
Anil Deshpande <anildesh82@gmail.com>
2024-05-02 21:15:01 +0530

    Adding working unit tests

--- app/src/test/java/com/codetutor/countryinfoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/codetutor/countryinfoapp/ExampleUnitTest.kt
@@ -1,6 +1,11 @@
 package com.codetutor.countryinfoapp
 
+import com.codetutor.countryinfoapp.database.Converters
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -9,8 +14,15 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
+
+
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
 }
