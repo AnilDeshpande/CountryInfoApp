7baef3317199473d36da1ece9752645e5bc26b45
Anil Deshpande <anildesh82@gmail.com>
2024-05-04 15:28:58 +0530

    adding couple of more queries

--- app/src/main/java/com/codetutor/countryinfoapp/database/CountryDao.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/database/CountryDao.kt
@@ -2,6 +2,7 @@ package com.codetutor.countryinfoapp.database
 
 import CountryEntity
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -12,6 +13,15 @@ interface CountryDao {
     @Query("SELECT * FROM Country")
     suspend fun getAllCountries(): List<Country>
 
+    @Query("SELECT * FROM Country WHERE continents LIKE :continent")
+    suspend fun getCountriesByContinent(continent: String): List<Country>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(countries: List<Country>)
+
+    @Delete
+    suspend fun delete(country: Country)
+
+    @Query("Update Country set capital = :capital where id = :id")
+    suspend fun updateCapital(capital: String, id: Int)
 }
