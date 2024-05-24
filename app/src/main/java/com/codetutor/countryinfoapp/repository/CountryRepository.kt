91007d390a2206bf61432c3301fa7790f0ebd522
Anil Deshpande <anildesh82@gmail.com>
2024-05-24 10:24:22 +0530

    removed the unncessary log files

--- app/src/main/java/com/codetutor/countryinfoapp/repository/CountryRepository.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/repository/CountryRepository.kt
@@ -17,15 +17,11 @@ class CountryRepository(private val context: Context, private val countryDao: Co
 
     suspend fun fetchAndInsertAll() = withContext(Dispatchers.IO) {
         if(getAllCountries() != null && getAllCountries().isNotEmpty()) {
-            Log.i("Room", "Countries already exist in the database ${getAllCountries().size}")
             return@withContext
 
         } else {
-            Log.i("Room", "Fetching and inserting countries")
             val mutableCountryList: MutableList<Country> = getCountryList(contextForRepo)
-            Log.i("Room", "Fetched countries: ${mutableCountryList.size}")
             val countryList: List<Country> = mutableCountryList.toList()
-            Log.i("Room", "Inserting Country list: ${countryList.size}")
             countryDao.insertAll(countryList)
             return@withContext
         }
@@ -33,30 +29,24 @@ class CountryRepository(private val context: Context, private val countryDao: Co
 
     suspend fun getAllCountries(): List<Country> = withContext(Dispatchers.IO) {
         if(allCountries.isNotEmpty()) {
-            Log.i("Room", "getAllCountries : ${allCountries.size}")
             return@withContext allCountries
         } else {
             allCountries = countryDao.getAllCountries()
-            Log.i("Room", "getAllCountries else : ${allCountries.size}")
             return@withContext allCountries
         }
     }
 
     suspend fun deleteCountry(country: Country) = withContext(Dispatchers.IO) {
-        Log.i("Room", " deleteCountry Countries Size before Deletion: ${allCountries.size}")
         countryDao.delete(country)
         allCountries = countryDao.getAllCountries()
-        Log.i("Room", "deleteCountry Countries Size after Deletion: ${allCountries.size}")
     }
 
     suspend fun updateCapital(country: Country, newCapital: String) = withContext(Dispatchers.IO) {
         val parsedString = "[\"${newCapital}\"]"
         val parsedArray = Json.decodeFromString<List<String>>(parsedString)
-        Log.i("Room", " updateCapital count: $parsedArray")
         //val count = countryDao.updateCapital(parsedArray, country?.id!!)
         val country = country?.copy(capital = parsedArray)
         countryDao.updateCountry(country!!)
         allCountries = countryDao.getAllCountries()
-
     }
 }
