8203cff7297c8b8a8b3f76a13e922c9d9677e69d
Anil Deshpande <anildesh82@gmail.com>
2024-05-24 10:02:50 +0530

    refactoring the app data base with better naming covention

--- app/src/main/java/com/codetutor/countryinfoapp/database/AppDataBase.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/database/AppDataBase.kt
@@ -7,16 +7,20 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.codetutor.countryinfoapp.data.Country
 
+interface DatabaseProvider {
+    fun countryDao(): CountryDao
+}
+
 @Database(entities = [Country::class], version = 1)
 @TypeConverters(Converters::class)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun countryDao(): CountryDao
+abstract class AppDatabase : RoomDatabase(), DatabaseProvider {
+    abstract override fun countryDao(): CountryDao
 
     companion object {
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+        private var INSTANCE: DatabaseProvider? = null
 
-        fun getDatabase(context: Context): AppDatabase {
+        fun getDatabase(context: Context): DatabaseProvider {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
