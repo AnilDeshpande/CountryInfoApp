f737e550e0d8ff0473b83816f3e84233570039ec
Anil Deshpande <anildesh82@gmail.com>
2024-06-11 11:55:13 +0530

    initial commit

---
+++ app/src/main/java/com/codetutor/countryinfoapp/repository/FilterByLanguage.kt
@@ -0,0 +1,9 @@
+package com.codetutor.countryinfoapp.repository
+
+import com.codetutor.countryinfoapp.data.Country
+
+class FilterByLanguage(private val language: String) : FilterCriteria {
+    override suspend fun filter(countries: List<Country>): List<Country> {
+        return countries.filter { it.languages?.nor.equals(language)}
+    }
+}
