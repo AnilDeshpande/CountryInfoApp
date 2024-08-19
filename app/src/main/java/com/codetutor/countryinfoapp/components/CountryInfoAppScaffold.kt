5f1c8656ee799b20e7beec1290cd0776e6bd6d9a
Anil Deshpande <anildesh82@gmail.com>
2024-08-19 22:10:58 +0530

    Working code ready

--- app/src/main/java/com/codetutor/countryinfoapp/components/CountryInfoAppScaffold.kt
+++ app/src/main/java/com/codetutor/countryinfoapp/components/CountryInfoAppScaffold.kt
@@ -41,6 +41,7 @@ import com.codetutor.countryinfoapp.repository.CountryRepository
 import com.codetutor.countryinfoapp.repository.FilterByContinent
 import com.codetutor.countryinfoapp.repository.FilterByDriveSide
 import com.codetutor.countryinfoapp.repository.service.CountryListProviderViaNetwork
+import com.codetutor.countryinfoapp.repository.service.CountryListServiceProvider
 import com.codetutor.countryinfoapp.repository.service.CountryListServiceProviderImpl
 import com.codetutor.countryinfoapp.viewmodel.CountryOperationViewModel
 import com.codetutor.countryinfoapp.viewmodel.CountryUIViewModel
@@ -56,7 +57,7 @@ fun CountryInfoAppScaffold(){
     val context = LocalContext.current
     val countryDao = AppDatabase.getDatabase(context.applicationContext).countryDao()
     /*val countryListProvider = CountryListServiceProviderImpl(context)*/
-    val countryListProvider = CountryListProviderViaNetwork()
+    val countryListProvider: CountryListServiceProvider = CountryListProviderViaNetwork()
     val repository = CountryRepository(countryDao,countryListProvider, Dispatchers.IO)
     val viewModelCountryOps: CountryOperationViewModel = viewModel(factory = CountryViewModelFactory(repository))
     val viewModelUI: CountryUIViewModel = viewModel { CountryUIViewModel(viewModelCountryOps) }
