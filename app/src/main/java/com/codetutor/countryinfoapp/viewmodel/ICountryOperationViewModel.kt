28ee2fcd05ecb13cc31244e707c1e2802a260293
Anil Deshpande <anildesh82@gmail.com>
2024-09-04 20:50:05 +0530

    initial commit

---
+++ app/src/main/java/com/codetutor/countryinfoapp/viewmodel/ICountryOperationViewModel.kt
@@ -0,0 +1,13 @@
+package com.codetutor.countryinfoapp.viewmodel
+
+import androidx.compose.runtime.MutableState
+import com.codetutor.countryinfoapp.data.Country
+import com.codetutor.countryinfoapp.repository.FilterCriteria
+
+interface ICountryOperationViewModel {
+    val allCountries: MutableState<List<Country>>
+    suspend fun getAllCountries()
+    suspend fun deleteCountry(country: Country)
+    suspend fun updateCapital(country: Country, newCapital: String)
+    suspend fun filterCountries(filterCriteria: FilterCriteria)
+}
