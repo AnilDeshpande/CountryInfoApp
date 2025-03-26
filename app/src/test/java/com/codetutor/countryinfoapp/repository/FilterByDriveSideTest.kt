e5842f9d638757742c8f097b9420544c22932f46
Anil Deshpande <anildesh82@gmail.com>
2025-03-26 16:16:34 +0530

    Video 5 - Android Unit Testing code commut

---
+++ app/src/test/java/com/codetutor/countryinfoapp/repository/FilterByDriveSideTest.kt
@@ -0,0 +1,30 @@
+package com.codetutor.countryinfoapp.repository
+
+import com.codetutor.countryinfoapp.data.Car
+import com.codetutor.countryinfoapp.data.Country
+import com.codetutor.countryinfoapp.data.Name
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+
+class FilterByDriveSideTest {
+
+    @Test
+    fun filterShouldReturnCountriesMatchingDriveSide()  = runBlocking {
+        //Arrange
+        val countries = listOf(
+            Country(name = Name("Country1"), car = Car(side = "left")),
+            Country(name = Name("Country2"), car = Car(side = "right")),
+            Country(name = Name("Country3"), car = Car(side = "left")),
+        )
+        //Act
+        val filter = FilterByDriveSide("left")
+        val leftFilteredCountries = filter.filter(countries)
+
+        //Assert
+        Assert.assertEquals(2,leftFilteredCountries.size)
+        leftFilteredCountries.forEach {
+            country -> assert(country.car?.side == "left")
+        }
+    }
+}
