a94c71aeeac1e406c9656096aff6004358fd8ad1
Anil Deshpande <anildesh82@gmail.com>
2025-06-26 22:02:58 +0530

    FakeCountryListServiceProvider added

---
+++ app/src/androidTest/java/com/codetutor/countryinfoapp/repository/FakeCountryListServiceProvider.kt
@@ -0,0 +1,21 @@
+package com.codetutor.countryinfoapp.repository
+
+import com.codetutor.countryinfoapp.data.Country
+import com.codetutor.countryinfoapp.repository.service.CountryListServiceProvider
+
+/**
+ * A fake implementation of CountryListServiceProvider for testing purposes.
+ * It returns a predefined list of countries instead of making actual API calls.
+ */
+class FakeCountryListServiceProvider(
+    private val testCountries: List<Country> = emptyList()
+) : CountryListServiceProvider {
+
+    /**
+     * Returns a mutable list of predefined test countries.
+     * @return MutableList<Country> The list of test countries.
+     */
+    override suspend fun getCountryList(): MutableList<Country> {
+        return testCountries.toMutableList()
+    }
+}
